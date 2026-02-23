b1ee3aaa8c17720a9832e1e4328ee18e4aa9f516
aashray2002 <162664267+aashray2002@users.noreply.github.com>
2026-02-23 11:39:38 +0530

    Refactor HomePage component structure and imports

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,29 +1,46 @@
-import Contact from "../../Components/Contact/Contact";
+import Navbar from "../../Components/Navbar/Navbar";
 import Hero from "../../Components/Hero/Hero";
 import Services from "../../Components/Services/Services";
-import { Element } from "react-scroll";
 import AboutUsTwo from "../../Components/AboutUsTwo/AboutUsTwo";
+import GallerySection from "../../Components/Gallery/GallerySection";
+import Contact from "../../Components/Contact/Contact";
+
+import { Element } from "react-scroll";
 import styles from "./homepage.module.css";
-import Navbar from "../../Components/Navbar/Navbar";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
-      <div className={styles.home}>
-        <Element name="home">
+
+      <main className={styles.home}>
+
+        {/* HERO SECTION */}
+        <Element name="home" className={styles.section}>
           <Hero />
         </Element>
-        <Element name="services">
+
+        {/* SERVICES SECTION */}
+        <Element name="services" className={styles.section}>
           <Services />
         </Element>
-        <Element name="about">
+
+        {/* ABOUT SECTION */}
+        <Element name="about" className={styles.section}>
           <AboutUsTwo />
         </Element>
-        <Element name="contact">
+
+        {/* GALLERY SECTION */}
+        <Element name="gallery" className={styles.section}>
+          <GallerySection />
+        </Element>
+
+        {/* CONTACT SECTION */}
+        <Element name="contact" className={styles.section}>
           <Contact />
         </Element>
-      </div>
+
+      </main>
     </>
   );
 }
